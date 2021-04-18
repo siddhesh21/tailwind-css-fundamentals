fb3a89bbe001fd468e1991b6e5b0afd84e4ada2a
Siddhesh Kankekar <kankekas@tcd.ie>
2021-04-18 18:30:25 +0100

    small fix

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Profile />
+
+      <div className="flex gap-x-5">
+        <button className="btn">Create</button>
+        <button
+          className="p-10 rounded-2xl shadow-lg ring-2 ring-gray-200 focus:outline-none 
+        focus:ring-blue-400 hover:ring-gray-500"
+        >
+          Help
+        </button>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./public/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx,vue}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
